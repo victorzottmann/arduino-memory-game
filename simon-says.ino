d76592ca0294291bccc12367dbe92642cf9bef7c
Victor Zottmann <zottmann.victor@gmail.com>
2023-04-24 13:32:03 +1000

    experimenting with blink sequences

--- simon-says.ino
+++ simon-says.ino
@@ -1,23 +1,53 @@
-#define redLed 2
+#define RED_LED 5
+#define GREEN_LED 4
+#define YELLOW_LED 3
+#define BLUE_LED 2
+
 int count = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(redLed, OUTPUT);
-  digitalWrite(redLed, LOW); // set the red LED pin to LOW when the program starts
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+
+  // set the LED pins to LOW when the program starts
+  digitalWrite(RED_LED, LOW); 
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
 }
 
 void blink(int ledPin) {
   digitalWrite(ledPin, HIGH);
-  count++;
-  Serial.println(count);
-  delay(1000);
+  delay(500);
   digitalWrite(ledPin, LOW);
+  delay(500);
+}
+
+void firstBlinkSequence() {
+  blink(RED_LED);
+  blink(GREEN_LED);
+  blink(YELLOW_LED);
+  blink(BLUE_LED);
+  delay(500);
+}
+
+void secondBlinkSequence() {
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(BLUE_LED, HIGH);
   delay(1000);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
+  delay(500);
 }
 
 void loop() {
-  while (count < 3) {
-    blink(redLed);
-  }
+  firstBlinkSequence();
+  secondBlinkSequence();
 }
